443c6365639ca189f7032cb5a83b5c916d72840e
Naobinario <mavasc.tablet@gmail.com>
2023-02-24 18:21:11 -0500

    Tarefa de hoje

---
+++ 20.02.2023 Exercicio 03.py
@@ -0,0 +1,7 @@
+nota = input("Me diz sua nota meninuuu: ")
+nome = input("Qual seu nome moleque: ")
+nota = float (nota)
+if nota >= 7 :
+    print ( "passou desgraça!!" )
+else :
+    print ( "Perdeu mané!!" )
